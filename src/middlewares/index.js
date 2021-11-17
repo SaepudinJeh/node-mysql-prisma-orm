1a170b8499e38ff9243bb783545566e04110fa16
Saepudin <saepudinjeh@gmail.com>
2021-11-17 15:00:30 +0700

    middlewares first check authenticated

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,6 +3,10 @@ const morgan = require('morgan');
 const compression = require('compression');
 const cors = require('cors');
 const express = require('express');
+const createError = require('http-errors');
+// Dependencies JWKS
+const expressJwt = require('express-jwt');
+const jwksClient = require('jwks-rsa');
 
 const verifyAccessToken = require('./verifyAccessToken');
 
@@ -13,6 +17,28 @@ module.exports = {
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
     app.use(morgan('dev'));
+
+    // First Check Authenticated
+    app.use(
+      expressJwt({
+        secret: jwksClient.expressJwtSecret({
+          jwksUri: 'https://api.ulin-app.xyz/auth/.well-known/jwks.json',
+          cache: true,
+          rateLimit: true,
+        }),
+        algorithms: ['RS256'],
+        credentialsRequired: true,
+      }),
+    );
+
+    // Handling Error Authenticated
+    app.use((err, req, res, next) => {
+      if (err.name === 'UnauthorizedError') {
+        res.status(err.status).send({ message: err.message });
+        return createError.Unauthorized();
+      }
+      next();
+    });
   },
   verifyAccessToken,
 };
